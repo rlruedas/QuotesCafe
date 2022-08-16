3d2b34cf2c64cb7881ed4855f3d68a5d4683a383
rlruedas <reister.ruedas@gmail.com>
2022-08-16 16:54:57 +0800

    Bump

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ function document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Tenor+Sans&family=Vollkorn:ital,wght@0,400;0,500;0,700;1,400&display=swap"
           rel="stylesheet"
